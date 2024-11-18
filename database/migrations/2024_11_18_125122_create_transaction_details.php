c0cdb1b6fa2ee6434c2a8711d78d6253092da734
Yafigg <Cipto.wongso88@gmail.com>
2024-11-18 20:01:05 +0700

    First Deploy

---
+++ database/migrations/2024_11_18_125122_create_transaction_details.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction-details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_transaksi');
+            $table->integer('id_produk');
+            $table->integer('qty');
+            $table->integer('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_transaksi');
+    }
+};
